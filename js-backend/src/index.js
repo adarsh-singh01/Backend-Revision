52075db46bb77755b6422627ad65bad2054a1b19
Adarsh Singh <adarshsince2004@gmail.com>
2024-08-09 14:39:56 +0530

    upto api response and error

---
+++ js-backend/src/index.js
@@ -0,0 +1,44 @@
+/*require('dotenv').config({path:'./env'})*/
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({path:'../env'})
+
+connectDB()
+.then(()=>{
+    app.listeb(process.env.PORT || 8000,()=>{
+        console.log(`server run at :${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection fail !!",err)
+})
+
+
+
+
+
+
+
+/*
+const app=express()
+//Jab bhi db connect kro use trycatch me daal do
+
+( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR here :",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`app chal rhi hai :${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR: ",error)
+        throw error
+    }
+} )()
+*/
---
+++ js-backend/src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err)=>next(err))
+    }
+}
+
+export{asyncHandler}
+
+//asyncHandler is a higher order func mtlb hum isme function bhi pass kr skte hai
+
+// const asyncHandler=()=>{}
+// const asyncHandler=(func)=>{()=>{}}//we can remove {}here
+// const asyncHandler=(func)=> async ()=>{}
+
+// const asyncHandler=(fn)=> async (req,res,next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
