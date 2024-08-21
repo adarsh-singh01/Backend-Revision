d4ed38e2794c8efb85051d1fa75fccd92c44e559
Adarsh Singh <adarshsince2004@gmail.com>
2024-08-22 01:59:41 +0530

    add models of tweet,playlist,like,comment

--- js-backend/src/controllers/user.controller.js
+++ js-backend/src/controllers/user.controller.js
@@ -154,10 +154,13 @@ const loginUser=asyncHandler(async (req,res)=>{
 const logoutUser=asyncHandler(async(req,res)=>{
     await User.findByIdAndUpdate(
         req.user._id,
-        {
-            $set:{
-                refreshToken:undefined
+        {   
+            $unset:{
+                refreshToken:1//this removes the field from doc
             }
+            // $set:{
+            //     refreshToken:undefined
+            // }
         },
         {
             new:true
